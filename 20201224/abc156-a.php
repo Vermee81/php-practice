52a5fd110758837821cf7123e8b394631da17eec
Vermee81 <hrksb5029@gmail.com>
2020-12-24 12:14:52 +0900

    [add] abc156aの問題を追加する

---
+++ 20201224/abc156-a.php
@@ -0,0 +1,25 @@
+<?php
+
+function calculate_rating($n, $r){
+    /*
+    問題文はこちらです
+    https://atcoder.jp/contests/abc156/tasks/abc156_a
+    ここに解答を書いてください。
+    */
+    return $ans;
+}
+
+function check_answer(){
+    if (calculate_rating(2, 2919) != 3719){
+        echo "Check 1 is wrong.";
+        return;
+    };
+    if (calculate_rating(22, 3051) != 3051){
+        echo "Check 2 is wrong";
+        return;
+    }
+    echo "All Passed!!!";
+}
+check_answer();
+
+?>
