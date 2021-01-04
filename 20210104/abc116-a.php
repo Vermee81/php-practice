38b38a80b4d7d4fdf124531e4f640fc81616c544
Vermee81 <hrksb5029@gmail.com>
2021-01-04 11:35:58 +0900

    [add] abc116aの問題を追加する

---
+++ 20210104/abc116-a.php
@@ -0,0 +1,27 @@
+<?php
+
+function calculate_area($ab, $bc, $ca){
+    // 問題文はこちらです
+    // https://atcoder.jp/contests/abc116/tasks/abc116_a
+    
+    return 6;
+}
+
+function check(){
+    if (calculate_area(3, 4, 5) != 6) {
+        echo "Check 1 is wrong";
+        return;
+    }
+    if (calculate_area(5, 12, 13) != 30) {
+        echo "Check 2 is wrong";
+        return;
+    }
+    if (calculate_area(45, 28, 53) != 630) {
+        echo "Check 3 is wrong";
+        return;
+    }
+    echo "All Passed!!!";
+}
+
+check();
+?>
